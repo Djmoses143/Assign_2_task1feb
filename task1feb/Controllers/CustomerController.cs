1c925af1a0d57a94b8dad6a16e06cdf1d467826b
john <john123@gmail.com>
2024-02-01 15:46:59 +0530

    Add project files.

---
+++ task1feb/Controllers/CustomerController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace task1feb.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            ViewBag.msg = "Customer Home Page";
+            return View();
+        }
+        public ActionResult CustomerList() {
+            List<string> list = new List<string>()
+            {
+                "Sam","Robin","Ramson"
+            };
+            ViewBag.nameList=list;
+            return View();
+        }
+    }
+}
